affb46ab8f7e9432591559f6294ea69b4174cddc
Valmor Tambosi Junior <junior.tambosi@gmail.com>
2026-01-21 22:59:17 -0300

    primeiro codigo

---
+++ playwright/e2e/online.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('webapp deve estar online ', async ({ page }) => {
+  await page.goto('http://localhost:5173');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/velo by papito/);
+});
+
+
