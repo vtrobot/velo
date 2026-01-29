45a40338720dbb0e092263f7f153eb0a0540d4c3
Valmor Tambosi Junior <junior.tambosi@gmail.com>
2026-01-29 14:10:05 -0300

    Como evitar repetição usando hooks de teste

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -2,71 +2,65 @@ import { test, expect } from '@playwright/test';
 
 import { gerarCodigoPedido } from '../support/Helpers'; 
 
-test('Deve consultar um pedido aprovado', async ({ page }) => {
+test.describe ('Consulta pedidos', ()=>{
 
-  //Test Data
+  test.beforeEach(async({page})=>{
 
-    const order ='VLO-B7ICS9'
-
-
-    //arrange
-  await page.goto('http://localhost:5173/');
-
-  //checkpoints
-  await expect(page.getByTestId('hero-section').getByRole('heading', { name: 'Velô Sprint' })).toBeVisible();
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+    await page.goto('http://localhost:5173/');
   
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-  //checkpoints texto consultar pedido
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
-
-  //act
-
-  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order);
-
-  await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+    //checkpoints
+    await expect(page.getByTestId('hero-section').getByRole('heading', { name: 'Velô Sprint' })).toBeVisible();
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+    
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+    //checkpoints texto consultar pedido
+    await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
 
-//assert
+  })
 
-  //checkpoints resultado do pedido
+  test('Deve consultar um pedido aprovado', async ({ page }) => {
 
-  // await expect(page.getByTestId('order-result-id')).toBeVisible({timeout:30_000});
-  // await expect(page.getByTestId('order-result-id')).toContainText('VLO-B7ICS9');
-
-  // const ordercode = page.locator('//p[text() ="Pedido"]/..//p[text()="VLO-B7ICS9"]')
-  // await expect(ordercode).toBeVisible({timeout:10_000});
-
-  const containerPedido = page.getByRole('paragraph')
-    .filter({ hasText: /^Pedido$/})
-    .locator('..') //sobe para elemento pai
-
-  await expect(containerPedido).toContainText(order,{timeout:10_000})
-
+    //Test Data
   
-  //checkpoints status do pedido
-  // await expect(page.getByTestId('order-result-status')).toBeVisible();
-  // await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+      const order ='VLO-B7ICS9'
+    //act
+  
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order);
+  
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+  
+  
+  //assert
+  
+    //checkpoints resultado do pedido
+  
+    // await expect(page.getByTestId('order-result-id')).toBeVisible({timeout:30_000});
+    // await expect(page.getByTestId('order-result-id')).toContainText('VLO-B7ICS9');
+  
+    // const ordercode = page.locator('//p[text() ="Pedido"]/..//p[text()="VLO-B7ICS9"]')
+    // await expect(ordercode).toBeVisible({timeout:10_000});
+  
+    const containerPedido = page.getByRole('paragraph')
+      .filter({ hasText: /^Pedido$/})
+      .locator('..') //sobe para elemento pai
+  
+    await expect(containerPedido).toContainText(order,{timeout:10_000})
+  
+    
+    //checkpoints status do pedido
+    // await expect(page.getByTestId('order-result-status')).toBeVisible();
+    // await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+  
+    await expect(page.getByText('APROVADO')).toBeVisible();
+  
+  });
+  
 
-  await expect(page.getByText('APROVADO')).toBeVisible();
-
-});
-
-test ('Deve validar quando não encontrar o pedido', async({page}) => {
+  test ('Deve validar quando não encontrar o pedido', async({page}) => {
 
     const order = gerarCodigoPedido()
 
-        //arrange
-  await page.goto('http://localhost:5173/');
-
-  //checkpoints
-  await expect(page.getByTestId('hero-section').getByRole('heading', { name: 'Velô Sprint' })).toBeVisible();
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-
-
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-  //checkpoints texto consultar pedido
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
   await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order);
   await page.getByRole('button', { name: 'Buscar Pedido' }).click();
@@ -87,8 +81,7 @@ test ('Deve validar quando não encontrar o pedido', async({page}) => {
 
 
 
-
-
+})
 
 
 
