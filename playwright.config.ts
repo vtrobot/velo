9f8e7e034d656dbdaa2dbbfbd90117a442f3d6c9
Valmor Tambosi Junior <junior.tambosi@gmail.com>
2026-06-12 23:59:10 -0300

    agora com o pull do vercel espero que funcione

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://velo-vtrobot-sigma.vercel.app/',
+    baseURL: 'https://velo-5r4lgc616-valmor-s-projects.vercel.app',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
