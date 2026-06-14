d41c1be9432d0e1fa7e93e72b83d9fda35042159
Valmor Tambosi Junior <junior.tambosi@gmail.com>
2026-06-13 23:31:42 -0300

    agora sim vamos ver se vai testar tudo

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://velo-5r4lgc616-valmor-s-projects.vercel.app',
+    baseURL: process.env.BASE_URL || 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
