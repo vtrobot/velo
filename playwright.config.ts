f92ab8b753c692546f0e66acc89a51f43c2e19b6
Valmor Tambosi Junior <junior.tambosi@gmail.com>
2026-06-16 23:27:01 -0300

    Desafio final

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,18 +40,18 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL || 'https://velo-oifoh9j11-valmor-s-projects.vercel.app/',
+    baseURL: process.env.BASE_URL || 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
 
     // Tempo máximo para ações interativas como click(), fill()
     // Quando o valor é 0, herda o limite do timeout geral do teste
-    actionTimeout: 5_000,
+    actionTimeout: 10_000,
 
     // Tempo máximo para navegações como goto(), waitForURL()
     // Quando o valor é 0, herda o limite do timeout geral do teste
-    navigationTimeout: 10_000
+    navigationTimeout: 15_000
   },
 
   /* Configure projects for major browsers */
